2f6bfc22124d16c09bc7455833220b2d1cfbd7a4
germfy <gdominguezmontes@gmail.com>
2016-08-16 00:40:34 -0500

    Prueba nueva

--- routes/pasos.js
+++ routes/pasos.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var cfenv = require('cfenv');
 var request = require('request');
 
-function enviarPush(token, texto, userID){
+function enviarPush(token, texto, userID, respuesta, cuerpo){
   request({
     url: "https://vhernanm-mfpf-server.mybluemix.net/imfpush/v1/apps/com.sample.pushnotificationscordovavhm/messages",
     headers : {
@@ -24,21 +24,25 @@ function enviarPush(token, texto, userID){
   }, function(error, response, body){
     if(error) {
         console.error(error);
-        return error;
+        respuesta = error;
     } else {
-        console.log(response.statusCode);
-        return response;
+        console.log("Codigo de respuestas" + response.statusCode);
+        respuesta = response;
+        cuerpo = body;
     }
   });
 };
 
 router.get('/paso0', function(req, res, next){
-	res.sendFile('mfpf_token.html');
+	res.sendFile('/public/mfpf_token.html');
 });
 
 router.get('/paso1', function(req, res, next){
   //enviarPush("eyJhbGciOiJSUzI1NiIsImp3ayI6eyJrdHkiOiJSU0EiLCJlIjoiQVFBQiIsImtpZCI6InRlc3QiLCJuIjoiQU0wRGQ3eEFkdjZILXlnTDdyOHFDTGRFLTNJMmtrNDV6Z1p0RGRfcXM4ZnZuWWZkaXFUU1Y0XzJ0Nk9HRzhDVjVDZTQxUE1wSXdtTDQxMFg5SVpudmh4b1lpRmNNU2FPZUlxb2UtckpBMHVadXcyckhoWFozV1ZDZUtlelJWY0NPWXNRTi1tUUswbWZ6NV8zby1ldjBVWXdYa1NPd0JCbDFFaHFJd1ZEd09pZWcySk1HbDBFWHNQWmZrTlpJLUhVNG9NaWktVHJOTHpSV2tNbUx2bTA5aEw1em9zVU5BMTV2ZUNLcGgyV3BtU20yUzYxbkRoSDdnTEVveW1EblRFalBZNUFvaDJpbkktMzZSR1lWTVVVYk80NkNyTlVZdUlvYk9pWGxMekJJaHVJQ3BmVmR4VF94N3N0S1g1QzlCZk1UQjRHa09IUDVjVXY3TnoxZERoSVB1OD0ifX0.eyJpc3MiOiJjb20uaWJtLm1mcCIsInN1YiI6InRlc3QiLCJhdWQiOiJjb20uaWJtLm1mcCIsImV4cCI6MTQ3MTMyMDE5NjAzNiwic2NvcGUiOiJwdXNoLmFwcGxpY2F0aW9uLmNvbS5zYW1wbGUucHVzaG5vdGlmaWNhdGlvbnNjb3Jkb3ZhdmhtIG1lc3NhZ2VzLndyaXRlIn0.XgXwD9yUkAywkUXzfA1nUM2uyuQZnbsxe9eVwGVhO1wCAP8qUj0KZ0h3zJo6Dpu78qS7CWuJfnQQ11bA2xMskeVP3sZ_a77OHWgIruE7F3BhZsVO9eqJrTjszwhmmE_ljdLLTTvA5XkJrMmDMhR7eUVCbrPFbFljMDopjCSL7NILsHXPfVjyDlFSiPX7aL7nP4jkcTyBG43EftyWV0yfHE_eS469JT1c4XvrS_m8XFMjevD0-dX1TkN0ThX21aLz_nfjNe6PXWcBa_ySmzgph-lqc2qwt0y_A25lDDNUpyfaT8AwiVrDJETjUhAvJxZ2i-ECClVxiReKuFjEt584XQ", "Prueba de paso 1", "germfy")
-  res.json(enviarPush(req.token, "Prueba de paso 1", req.userID));
+  enviarPush(req.token, "Prueba de paso 1", req.userID, respuesta, cuerpo, function(req, res, next){
+    res.send(respuesta);
+  }
+  );
 });
 
 module.exports = router;
